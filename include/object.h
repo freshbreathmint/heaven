0f07f07735973e89f942f726abf20d6c36c26422
freshbreathmint <33765528+freshbreathmint@users.noreply.github.com>
2023-11-28 18:24:02 -0800

    merge geometry and object into one struct

--- include/object.h
+++ include/object.h
@@ -1,12 +1,21 @@
 #ifndef OBJECT_H
 #define OBJECT_H
 
+#include <stdlib.h>
+
 typedef struct Object {
+    // Buffers
     unsigned int VAO;
     unsigned int VBO;
     unsigned int EBO;
+
+    // Geometry
+    float *vertices;
+    unsigned int *indices;
+    size_t numVertices;
+    size_t numIndices;
 } Object;
 
-void createObject(Object *object, Geometry *geometry);
+void createObject(Object *object, const char *filename);
 
 #endif // OBJECT_H
--- src/object.c
+++ src/object.c
@@ -1,30 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+
 #include <glad/glad.h>
 
-#include "geometry.h"
 #include "object.h"
 
-void createObject(Object *object, Geometry *geometry)
+//this needs to be rewritten again
+void readGeometry(Object *object, const char *filename)
+{
+    // Open the file
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Failed to open file: %s\n", filename);
+        return;
+    }
+
+    // Buffer to hold the value read from the file
+    char valueStr[32];
+    size_t numVertices = 0;
+    size_t numIndices = 0;
+
+    // First pass: count vertices and indices
+    while (fscanf(file, "%s", valueStr) == 1)
+    {
+        // If the value contains a decimal point, it's a vertex
+        if (strchr(valueStr, '.') != NULL)
+        {
+            numVertices++;
+        }
+        // If the value doesn't contain a decimal point, it's an index
+        else
+        {
+            
+            numIndices++;
+        }
+    }
+
+    // Allocate memory for vertices and indices
+    object->vertices = malloc(numVertices * sizeof(float));
+    object->indices = malloc(numIndices * sizeof(unsigned int));
+
+    // Check if memory allocation was successful
+    if (object->vertices == NULL || object->indices == NULL)
+    {
+        printf("Memory allocation failed\n");
+        fclose(file);
+        return;
+    }
+
+    // Reset file pointer to the beginning
+    rewind(file);
+
+    size_t vertexIndex = 0;
+    size_t indexIndex = 0;
+
+    // Second pass: populate arrays
+    while (fscanf(file, "%s", valueStr) == 1)
+    {
+        // If the value contains a decimal point, it's a vertex
+        if (strchr(valueStr, '.') != NULL)
+        {
+            object->vertices[vertexIndex++] = atof(valueStr);
+        }
+        // If the value doesn't contain a decimal point, it's an index
+        else
+        {
+            
+            object->indices[indexIndex++] = atoi(valueStr);
+        }
+    }
+
+    // Update Object
+    object->numVertices = numVertices;
+    object->numIndices = numIndices;
+
+    // Close the file
+    fclose(file);
+}
+
+void createObject(Object *object, const char *filename)
 {
     // Generate Object Arrays/Buffers
     glGenVertexArrays(1, &object->VAO);
     glGenBuffers(1, &object->VBO);
     glGenBuffers(1, &object->EBO);
 
+    // Read Geometry Data
+    readGeometry(object, filename);
+
     // Bind Array
     glBindVertexArray(object->VAO);
 
     // Bind Buffers To Array & Buffer Data
     glBindBuffer(GL_ARRAY_BUFFER, object->VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * geometry->numVertices, geometry->vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * object->numVertices, object->vertices, GL_STATIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, object->EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * geometry->numIndices, geometry->indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * object->numIndices, object->indices, GL_STATIC_DRAW);
 
     // Link Vertex Attributes
     // NOTE: This needs to be changed later to optimize itself based on what kind of Geometry Data there is, alternateively use the standard. Idk which.
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    //Unbind Array & Free Geometry
+    //Unbind Array & Free Geometry Data
     glBindVertexArray(0);
-    freeGeometry(geometry);
+    free(object->vertices);
+    free(object->indices);
 }
